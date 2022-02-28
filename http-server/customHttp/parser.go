a757877a3b9f2061bd8e78820915b4a6d82db515
svetlimladenov <svetlimladenov@gmail.com>
2022-02-28 17:37:00 +0200

    Create customHttp package

---
+++ http-server/customHttp/parser.go
@@ -0,0 +1,29 @@
+package custom_http
+
+import "errors"
+
+type HttpMethod int64
+
+const (
+	GET HttpMethod = iota
+	POST
+	PUT
+	PATCH
+	DELETE
+)
+
+type HttpRequst struct {
+	Method HttpMethod
+	Body   string
+}
+
+func parseHttpRequest(req string) (HttpRequst, error) {
+	if req == "" {
+		return HttpRequst{}, errors.New("the request body cannot be empty")
+	}
+
+	return HttpRequst{
+		Method: GET,
+		Body:   req,
+	}, nil
+}
---
+++ http-server/customHttp/server.go
@@ -0,0 +1,8 @@
+package custom_http
+
+import "fmt"
+
+func Listen(host string, port int) error {
+	fmt.Println("Will listen here")
+	return nil
+}
